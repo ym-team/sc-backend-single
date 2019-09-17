1a985a58a83f61a45eb14ee4d97f22a2f1e89672
xufuzhou <753911752@qq.com>
2019-09-17 23:45:51 +0800

    医生管理

--- src/main/java/com/cloud/business/hospital/HospitalMapper.java
+++ src/main/java/com/cloud/business/hospital/HospitalMapper.java
@@ -1,12 +1,12 @@
 package com.cloud.business.hospital;
 
-import java.util.List;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import com.github.pagehelper.Page;
+
+import java.util.List;
 
 /**
- *
  * 医院表
  *
  * @author Michael
@@ -15,86 +15,91 @@ import com.github.pagehelper.Page;
 @Mapper
 public interface HospitalMapper {
 
-	 /**
-	   * 新增 医院表 信息
-	   * @param HospitalModel
-	   * @return int
-	   * @throws Exception
-	   */
-	public int save(@Param("hospitalModel")HospitalModel  hospitalModel);
-	
-	
+    /**
+     * 新增 医院表 信息
+     *
+     * @param HospitalModel
+     * @return int
+     * @throws Exception
+     */
+    public int save(@Param("hospitalModel") HospitalModel hospitalModel);
 
-	/**
-	   *批量新增
-	   * @param List<HospitalModel>
-	   * @return int
-	   * @throws Exception
-	   */
-	public int saveList(@Param("listHospitalModel")List<HospitalModel>  listHospitalModel);
-	
 
-  	 /**
-	   * 查询详情
-	   * @param HospitalModel
-	   * @return HospitalModel
-	   * @throws Exception
-	   */
-	public HospitalModel queryDetail(@Param("hospitalModel")HospitalModel hospitalModel);
-  
-	/**
-	 * 查询列表
-	 * @param HospitalModel
-	 * @return List<HospitalModel>
-	 * @throws Exception
-	 */
-	public List<HospitalModel> queryList(@Param("hospitalModel")HospitalModel hospitalModel);
-	  
-	  
-	/**
-	 * 分页查询列表
-	 * @param HospitalModel
-	 * @return Page<HospitalModel>
-	 * @throws Exception
-	 */
-	public Page<HospitalModel> queryListByPage(@Param("hospitalModel")HospitalModel hospitalModel);
-	  
-	  
-	/**
-	 * 更新
-	 * @param HospitalModel
-	 * @return int
-	 * @throws Exception
-	 */
-	public int update(@Param("hospitalModel")HospitalModel hospitalModel);
-	
-	
-	/**
-	 * 批量更新
-	 * @param List<HospitalModel>
-	 * @return int
-	 * @throws Exception
-	 */
-	public int updateList(@Param("listHospitalModel")List<HospitalModel> listHospitalModel);
-	  
-	  
-	    
-	 /**
-	  * 物理删除 
-	  * @param HospitalModel
-	  * @return int
-	  * @throws Exception
-	  */
-	public int delete(@Param("hospitalModel")HospitalModel hospitalModel);
-	
-	/**
-	  * 根据入参条件查询 总数
-	  * @param HospitalModel
-	  * @throws Exception
-	  */
-	public Long queryCount(@Param("hospitalModel")HospitalModel hospitalModel);
-	
-	
-	  
+    /**
+     * 批量新增
+     *
+     * @param List<HospitalModel>
+     * @return int
+     * @throws Exception
+     */
+    public int saveList(@Param("listHospitalModel") List<HospitalModel> listHospitalModel);
+
+
+    /**
+     * 查询详情
+     *
+     * @param HospitalModel
+     * @return HospitalModel
+     * @throws Exception
+     */
+    public HospitalModel queryDetail(@Param("hospitalModel") HospitalModel hospitalModel);
+
+    /**
+     * 查询列表
+     *
+     * @param HospitalModel
+     * @return List<HospitalModel>
+     * @throws Exception
+     */
+    public List<HospitalModel> queryList(@Param("hospitalModel") HospitalModel hospitalModel);
+
+
+    /**
+     * 分页查询列表
+     *
+     * @param HospitalModel
+     * @return Page<HospitalModel>
+     * @throws Exception
+     */
+    public Page<HospitalModel> queryListByPage(@Param("hospitalModel") HospitalModel hospitalModel);
+
+
+    /**
+     * 更新
+     *
+     * @param HospitalModel
+     * @return int
+     * @throws Exception
+     */
+    public int update(@Param("hospitalModel") HospitalModel hospitalModel);
+
+
+    /**
+     * 批量更新
+     *
+     * @param List<HospitalModel>
+     * @return int
+     * @throws Exception
+     */
+    public int updateList(@Param("listHospitalModel") List<HospitalModel> listHospitalModel);
+
+
+    /**
+     * 物理删除
+     *
+     * @param HospitalModel
+     * @return int
+     * @throws Exception
+     */
+    public int delete(@Param("hospitalModel") HospitalModel hospitalModel);
+
+    /**
+     * 根据入参条件查询 总数
+     *
+     * @param HospitalModel
+     * @throws Exception
+     */
+    public Long queryCount(@Param("hospitalModel") HospitalModel hospitalModel);
+
 
 }
--- src/main/java/com/cloud/business/hospitalDoctor/HospitalDoctorServiceImpl.java
+++ src/main/java/com/cloud/business/hospitalDoctor/HospitalDoctorServiceImpl.java
@@ -1,156 +1,158 @@
 package com.cloud.business.hospitalDoctor;
 
 /**
-import com.cloud.business.HospitalDoctor.HospitalDoctorModel;
-import com.cloud.business.HospitalDoctor.HospitalDoctorMapper;
-import com.cloud.business.HospitalDoctor.HospitalDoctorService;
-*/
+ * import com.cloud.business.HospitalDoctor.HospitalDoctorModel;
+ * import com.cloud.business.HospitalDoctor.HospitalDoctorMapper;
+ * import com.cloud.business.HospitalDoctor.HospitalDoctorService;
+ */
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.cloud.user.model.LoginAppUser;
+import com.cloud.util.AppUserUtil;
 import com.cloud.util.BeanConvertUtils;
 import com.cloud.util.page.PageInfo;
 import com.cloud.util.page.PageQuery;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
  * ${comments} 服务实现类
  * </p>
- 
  *
  * @author Michael
- * @date  2019-09-17 01:13:28
+ * @date 2019-09-17 01:13:28
  */
 @Slf4j
 @Service
-public class HospitalDoctorServiceImpl implements IHospitalDoctorService  {
+public class HospitalDoctorServiceImpl implements IHospitalDoctorService {
 
 
-	  @Autowired
-	  private HospitalDoctorMapper hospitalDoctorMapper;
-  
-  
-     /**
-	   * 新增
-	   * @param HospitalDoctorModel
-	   * @return long
-	 */
-	  @Override
-	  public long save(HospitalDoctorBOS hospitalDoctorBOS) throws Exception{
-	  	HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOS, HospitalDoctorModel.class);
-		int count = this.hospitalDoctorMapper.save(hospitalDoctorModel);
-	    return hospitalDoctorModel.getId();
-	  }
-  
-	   /**
-		 *批量新增
-		 * @param List<HospitalDoctorBOS>
-		 * @return	int
-		*/
-	  @Override
-	  public int saveList(List<HospitalDoctorBOS> listHospitalDoctorBOS) throws Exception {
-	  	List<HospitalDoctorModel> listHospitalDoctorModel = BeanConvertUtils.convertList(listHospitalDoctorBOS, HospitalDoctorModel.class);
-		int count = this.hospitalDoctorMapper.saveList(listHospitalDoctorModel);
-	    return count;
-	  }
-
-  
-  
-  	/**
-	   *查询详情
-	   * @param HospitalDoctorBOQ
-	   * @return HospitalDoctorVO
-	   */
-	@Override
-	public HospitalDoctorVO queryDetail(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
-		hospitalDoctorModel = this.hospitalDoctorMapper.queryDetail(hospitalDoctorModel);
-		return BeanConvertUtils.convert(hospitalDoctorModel, HospitalDoctorVO.class);
-	}
-  
-	/**
-	 * 
-	 * @param HospitalDoctorBOQ
-	 * @return	List<HospitalDoctorVO>
-	 * ${table.JavaName}
-	 */
-	@Override
-	public List<HospitalDoctorVO> queryList(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
-		List<HospitalDoctorModel> listHospitalDoctorModel = this.hospitalDoctorMapper.queryList(hospitalDoctorModel);
-		return  BeanConvertUtils.convertList(listHospitalDoctorModel, HospitalDoctorVO.class);
-	}
-	  
-	
-	/**
-	 * 
-	 * @param HospitalDoctorBOQP
-	 * @param page
-	 * @return PageQuery<HospitalDoctorVO>
-	 */
-	@Override
-	public PageQuery<HospitalDoctorVO> queryListByPage(HospitalDoctorBOQP hospitalDoctorBOQP,PageInfo page) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQP, HospitalDoctorModel.class);
-		PageHelper.startPage(page.getCurrPage(), page.getPageSize(), page.getOrderby());
-		Page<HospitalDoctorModel> queryListByPage = this.hospitalDoctorMapper.queryListByPage(hospitalDoctorModel);
-		return  new PageQuery(queryListByPage, HospitalDoctorVO.class);
-	}
-	
-	
-	  
-	/**
-	 *
-	 * @param HospitalDoctorBOU
-	 * @return int
-	 */
-	@Override
-	public int update(HospitalDoctorBOU hospitalDoctorBOU) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOU, HospitalDoctorModel.class);
-		return this.hospitalDoctorMapper.update(hospitalDoctorModel);
-	}
-	
-	
-	/**
-	 *
-	 * @param List<HospitalDoctorBOU>
-	 * @return int
-	 */
-	@Override
-	public int updateList(List<HospitalDoctorBOU> listHospitalDoctorBOU) throws Exception{
-		List<HospitalDoctorModel> listHospitalDoctorModel = BeanConvertUtils.convertList(listHospitalDoctorBOU, HospitalDoctorModel.class);
-		return this.hospitalDoctorMapper.updateList(listHospitalDoctorModel);
-	}
-	
-	  
-	 /**
-	  * 
-	  * @param HospitalDoctorBOD
-	  * @return int
-	  */
-	  @Override
-	public int delete(HospitalDoctorBOD hospitalDoctorBOD) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOD, HospitalDoctorModel.class);
-		return this.hospitalDoctorMapper.delete(hospitalDoctorModel);
-	}
-	
-	/**
-	  * 
-	  * @param HospitalDoctorBOQ
-	  * @return Long
-	  */
-	 @Override
-	public Long queryCount(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception{
-		HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
-		return this.hospitalDoctorMapper.queryCount(hospitalDoctorModel);
-	}
+    @Autowired
+    private HospitalDoctorMapper hospitalDoctorMapper;
 
 
-	
+    /**
+     * 新增
+     *
+     * @param HospitalDoctorModel
+     * @return long
+     */
+    @Override
+    public long save(HospitalDoctorBOS hospitalDoctorBOS) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOS, HospitalDoctorModel.class);
+        LoginAppUser user = AppUserUtil.getLoginAppUser();
+        hospitalDoctorModel.setCreateId(user.getId());
+        hospitalDoctorModel.setUpdateId(user.getId());
+        //hospitalModel.setCreateTime(new Date());
+        //hospitalModel.setUpdateTime(new Date());
+        int count = this.hospitalDoctorMapper.save(hospitalDoctorModel);
+        return hospitalDoctorModel.getId();
+    }
+
+    /**
+     * 批量新增
+     *
+     * @param List<HospitalDoctorBOS>
+     * @return int
+     */
+    @Override
+    public int saveList(List<HospitalDoctorBOS> listHospitalDoctorBOS) throws Exception {
+        List<HospitalDoctorModel> listHospitalDoctorModel = BeanConvertUtils.convertList(listHospitalDoctorBOS, HospitalDoctorModel.class);
+        int count = this.hospitalDoctorMapper.saveList(listHospitalDoctorModel);
+        return count;
+    }
+
+
+    /**
+     * 查询详情
+     *
+     * @param HospitalDoctorBOQ
+     * @return HospitalDoctorVO
+     */
+    @Override
+    public HospitalDoctorVO queryDetail(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
+        hospitalDoctorModel = this.hospitalDoctorMapper.queryDetail(hospitalDoctorModel);
+        return BeanConvertUtils.convert(hospitalDoctorModel, HospitalDoctorVO.class);
+    }
+
+    /**
+     * @param HospitalDoctorBOQ
+     * @return List<HospitalDoctorVO>
+     * ${table.JavaName}
+     */
+    @Override
+    public List<HospitalDoctorVO> queryList(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
+        List<HospitalDoctorModel> listHospitalDoctorModel = this.hospitalDoctorMapper.queryList(hospitalDoctorModel);
+        return BeanConvertUtils.convertList(listHospitalDoctorModel, HospitalDoctorVO.class);
+    }
+
+
+    /**
+     * @param HospitalDoctorBOQP
+     * @param page
+     * @return PageQuery<HospitalDoctorVO>
+     */
+    @Override
+    public PageQuery<HospitalDoctorVO> queryListByPage(HospitalDoctorBOQP hospitalDoctorBOQP, PageInfo page) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQP, HospitalDoctorModel.class);
+        PageHelper.startPage(page.getCurrPage(), page.getPageSize(), page.getOrderby());
+        Page<HospitalDoctorModel> queryListByPage = this.hospitalDoctorMapper.queryListByPage(hospitalDoctorModel);
+        return new PageQuery(queryListByPage, HospitalDoctorVO.class);
+    }
+
+
+    /**
+     * @param HospitalDoctorBOU
+     * @return int
+     */
+    @Override
+    public int update(HospitalDoctorBOU hospitalDoctorBOU) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOU, HospitalDoctorModel.class);
+        LoginAppUser user = AppUserUtil.getLoginAppUser();
+        hospitalDoctorModel.setUpdateId(user.getId());
+        //hospitalModel.setUpdateTime(new Date());
+
+        return this.hospitalDoctorMapper.update(hospitalDoctorModel);
+    }
+
+
+    /**
+     * @param List<HospitalDoctorBOU>
+     * @return int
+     */
+    @Override
+    public int updateList(List<HospitalDoctorBOU> listHospitalDoctorBOU) throws Exception {
+        List<HospitalDoctorModel> listHospitalDoctorModel = BeanConvertUtils.convertList(listHospitalDoctorBOU, HospitalDoctorModel.class);
+        return this.hospitalDoctorMapper.updateList(listHospitalDoctorModel);
+    }
+
+
+    /**
+     * @param HospitalDoctorBOD
+     * @return int
+     */
+    @Override
+    public int delete(HospitalDoctorBOD hospitalDoctorBOD) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOD, HospitalDoctorModel.class);
+        return this.hospitalDoctorMapper.delete(hospitalDoctorModel);
+    }
+
+    /**
+     * @param HospitalDoctorBOQ
+     * @return Long
+     */
+    @Override
+    public Long queryCount(HospitalDoctorBOQ hospitalDoctorBOQ) throws Exception {
+        HospitalDoctorModel hospitalDoctorModel = BeanConvertUtils.convert(hospitalDoctorBOQ, HospitalDoctorModel.class);
+        return this.hospitalDoctorMapper.queryCount(hospitalDoctorModel);
+    }
+
+
 }
