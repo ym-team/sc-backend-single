e4a882a63b04c5026fbbbc25a392d07f5aea35c0
xufuzhou <753911752@qq.com>
2019-09-18 21:54:29 +0800

    客户管理

--- src/main/java/com/cloud/business/cusconsumer/CusconsumerMapper.java
+++ src/main/java/com/cloud/business/cusconsumer/CusconsumerMapper.java
@@ -1,12 +1,12 @@
 package com.cloud.business.cusconsumer;
 
-import java.util.List;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import com.github.pagehelper.Page;
+
+import java.util.List;
 
 /**
- *
  * 顾客信息
  *
  * @author Michael
@@ -15,86 +15,80 @@ import com.github.pagehelper.Page;
 @Mapper
 public interface CusconsumerMapper {
 
-	 /**
-	   * 新增 顾客信息 信息
-	   * @param CusconsumerModel
-	   * @return int
-	   * @throws Exception
-	   */
-	public int save(@Param("cusconsumerModel")CusconsumerModel  cusconsumerModel);
-	
-	
+    /**
+     * 新增 顾客信息 信息
+     *
+     * @param cusconsumerModel
+     * @return int
+     */
+    int save(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
 
-	/**
-	   *批量新增
-	   * @param List<CusconsumerModel>
-	   * @return int
-	   * @throws Exception
-	   */
-	public int saveList(@Param("listCusconsumerModel")List<CusconsumerModel>  listCusconsumerModel);
-	
 
-  	 /**
-	   * 查询详情
-	   * @param CusconsumerModel
-	   * @return CusconsumerModel
-	   * @throws Exception
-	   */
-	public CusconsumerModel queryDetail(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-  
-	/**
-	 * 查询列表
-	 * @param CusconsumerModel
-	 * @return List<CusconsumerModel>
-	 * @throws Exception
-	 */
-	public List<CusconsumerModel> queryList(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-	  
-	  
-	/**
-	 * 分页查询列表
-	 * @param CusconsumerModel
-	 * @return Page<CusconsumerModel>
-	 * @throws Exception
-	 */
-	public Page<CusconsumerModel> queryListByPage(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-	  
-	  
-	/**
-	 * 更新
-	 * @param CusconsumerModel
-	 * @return int
-	 * @throws Exception
-	 */
-	public int update(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-	
-	
-	/**
-	 * 批量更新
-	 * @param List<CusconsumerModel>
-	 * @return int
-	 * @throws Exception
-	 */
-	public int updateList(@Param("listCusconsumerModel")List<CusconsumerModel> listCusconsumerModel);
-	  
-	  
-	    
-	 /**
-	  * 物理删除 
-	  * @param CusconsumerModel
-	  * @return int
-	  * @throws Exception
-	  */
-	public int delete(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-	
-	/**
-	  * 根据入参条件查询 总数
-	  * @param CusconsumerModel
-	  * @throws Exception
-	  */
-	public Long queryCount(@Param("cusconsumerModel")CusconsumerModel cusconsumerModel);
-	
-	
-	  
+    /**
+     * 批量新增
+     *
+     * @param listCusconsumerModel
+     * @return int
+     */
+    int saveList(@Param("listCusconsumerModel") List<CusconsumerModel> listCusconsumerModel);
 
+
+    /**
+     * 查询详情
+     *
+     * @param cusconsumerModel
+     * @return CusconsumerModel
+     */
+    CusconsumerModel queryDetail(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
+
+    /**
+     * 查询列表
+     *
+     * @param cusconsumerModel
+     * @return List<CusconsumerModel>
+     */
+    List<CusconsumerModel> queryList(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
+
+
+    /**
+     * 分页查询列表
+     *
+     * @param cusconsumerModel
+     * @return Page<CusconsumerModel>
+     */
+    Page<CusconsumerModel> queryListByPage(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
+
+
+    /**
+     * 更新
+     *
+     * @param cusconsumerModel
+     * @return int
+     */
+    int update(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
+
+
+    /**
+     * 批量更新
+     *
+     * @param listCusconsumerModel
+     * @return int
+     */
+    int updateList(@Param("listCusconsumerModel") List<CusconsumerModel> listCusconsumerModel);
+
+
+    /**
+     * 物理删除
+     *
+     * @param cusconsumerModel
+     * @return int
+     */
+    int delete(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
+
+    /**
+     * 根据入参条件查询 总数
+     *
+     * @param cusconsumerModel
+     */
+    Long queryCount(@Param("cusconsumerModel") CusconsumerModel cusconsumerModel);
 }
--- src/main/java/com/cloud/business/cusconsumer/ICusconsumerService.java
+++ src/main/java/com/cloud/business/cusconsumer/ICusconsumerService.java
@@ -1,14 +1,10 @@
 package com.cloud.business.cusconsumer;
 
 
-import java.util.List;
 import com.cloud.util.page.PageInfo;
 import com.cloud.util.page.PageQuery;
-import com.github.pagehelper.Page;
 
-
-import com.cloud.business.cusconsumer.CusconsumerModel;
-
+import java.util.List;
 
 
 /**
@@ -20,89 +16,88 @@ import com.cloud.business.cusconsumer.CusconsumerModel;
  * @since 2019-09-17 01:13:26
  */
 public interface ICusconsumerService {
+    /**
+     * 新增 顾客信息 信息
+     *
+     * @param cusconsumerBOS
+     * @return int
+     * @throws Exception
+     */
+    long save(CusconsumerBOS cusconsumerBOS) throws Exception;
+
+    /**
+     * 批量新增
+     *
+     * @param listCusconsumerBOS
+     * @return int
+     * @throws Exception
+     */
+    int saveList(List<CusconsumerBOS> listCusconsumerBOS) throws Exception;
+
+    /**
+     * 查询详情
+     *
+     * @param cusconsumerBOQ
+     * @return CusconsumerVO
+     * @throws Exception
+     */
+    CusconsumerVO queryDetail(CusconsumerBOQ cusconsumerBOQ) throws Exception;
+
+    /**
+     * 查询列表
+     *
+     * @param cusconsumerBOQ
+     * @return List<CusconsumerVO>
+     * @throws Exception
+     */
+    List<CusconsumerVO> queryList(CusconsumerBOQ cusconsumerBOQ) throws Exception;
 
 
-	 /**
-	   * 新增 顾客信息 信息
-	   * @param CusconsumerBOS
-	   * @return int
-	   * @throws Exception
-	   */
-	public long save(CusconsumerBOS  cusconsumerBOS)throws Exception;
-	
-	
+    /**
+     * 分页查询列表
+     *
+     * @param cusconsumerBOQP
+     * @return PageQuery<CusconsumerVO>
+     * @throws Exception
+     */
+    PageQuery<CusconsumerVO> queryListByPage(CusconsumerBOQP cusconsumerBOQP, PageInfo page) throws Exception;
 
-	/**
-	   *批量新增
-	   * @param List<CusconsumerBOS>
-	   * @return int
-	   * @throws Exception
-	   */
-	public int saveList(List<CusconsumerBOS>  listCusconsumerBOS)throws Exception;
-	
 
-  	 /**
-	   * 查询详情
-	   * @param CusconsumerBOQ
-	   * @return CusconsumerVO
-	   * @throws Exception
-	   */
-	public CusconsumerVO queryDetail(CusconsumerBOQ cusconsumerBOQ)throws Exception;
-  
-	/**
-	 * 查询列表
-	 * @param CusconsumerBOQ
-	 * @return List<CusconsumerVO>
-	 * @throws Exception
-	 */
-	public List<CusconsumerVO> queryList(CusconsumerBOQ cusconsumerBOQ)throws Exception;
-	  
-	  
-	/**
-	 * 分页查询列表
-	 * @param CusconsumerBOQP
-	 * @return PageQuery<CusconsumerVO>
-	 * @throws Exception
-	 */
-	public PageQuery<CusconsumerVO> queryListByPage(CusconsumerBOQP cusconsumerBOQP,PageInfo page)throws Exception;
-	  
-	  
-	/**
-	 * 更新
-	 * @param CusconsumerBOU
-	 * @return int
-	 * @throws Exception
-	 */
-	public int update(CusconsumerBOU cusconsumerBOU)throws Exception;
-	
-	
-	/**
-	 * 批量更新
-	 * @param List<CusconsumerBOU>
-	 * @return int
-	 * @throws Exception
-	 */
-	public int updateList(List<CusconsumerBOU> listCusconsumerBOU)throws Exception;
-	  
-	  
-	    
-	 /**
-	  * 物理删除 
-	  * @param CusconsumerBOD
-	  * @return int
-	  * @throws Exception
-	  */
-	public int delete(CusconsumerBOD cusconsumerBOD)throws Exception;
-	
-	/**
-	  * 根据入参条件查询 总数
-	  * @param CusconsumerBOQ
-	  * @return Long
-	  * @throws Exception
-	  */
-	public Long queryCount(CusconsumerBOQ cusconsumerBOQ)throws Exception;
-	
-	
-	  
+    /**
+     * 更新
+     *
+     * @param cusconsumerBOU
+     * @return int
+     * @throws Exception
+     */
+    int update(CusconsumerBOU cusconsumerBOU) throws Exception;
 
+
+    /**
+     * 批量更新
+     *
+     * @param listCusconsumerBOU
+     * @return int
+     * @throws Exception
+     */
+    int updateList(List<CusconsumerBOU> listCusconsumerBOU) throws Exception;
+
+
+    /**
+     * 物理删除
+     *
+     * @param cusconsumerBOD
+     * @return int
+     * @throws Exception
+     */
+    int delete(CusconsumerBOD cusconsumerBOD) throws Exception;
+
+    /**
+     * 根据入参条件查询 总数
+     *
+     * @param cusconsumerBOQ
+     * @return Long
+     * @throws Exception
+     */
+    Long queryCount(CusconsumerBOQ cusconsumerBOQ) throws Exception;
 }
