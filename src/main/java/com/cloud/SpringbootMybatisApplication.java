fe37b1e6e2b5c40c65ec64ec04ee9a8a70852baa
michael <mengdekaituozhe@163.com>
2019-09-20 00:22:22 +0800

    限流策略

--- src/main/java/com/cloud/SpringbootMybatisApplication.java
+++ src/main/java/com/cloud/SpringbootMybatisApplication.java
@@ -18,13 +18,13 @@ public class SpringbootMybatisApplication {
 	
 	
 	/**
-	 * 差引入ehcache  
+	 * 差引入ehcache  		https://www.cnblogs.com/yueshutong/p/9381540.html
 	 * 监控中心
 	 * 还有一些工具类,aop 异步 log 
 	 * 
 	 * 页面还差一个多选删除，还差一个排序,差excel上传和下载，详情页（直接用update页）
 	 * 还差一个加解密 传输
-	 * aop 异步 log + traceId	 ：https://www.jianshu.com/p/08f5231c3d32   https://www.cnblogs.com/baobaoxiaokeai/p/11052738.html
+	 * aop 异步 log + traceId	 ：https://www.jianshu.com/p/08f5231c3d32   https://www.cnblogs.com/baobaoxiaokeai/p/11052738.html  未同步
 	 * 阻止二次提交 表单重复提交
 	 * crf之类
 	 * 限流策略
--- src/main/java/com/cloud/business/count/CountController.java
+++ src/main/java/com/cloud/business/count/CountController.java
@@ -1,6 +1,34 @@
 package com.cloud.business.count;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cloud.business.bussinessSum.BussinessSumBOQ;
+import com.cloud.business.bussinessSum.IBussinessSumService;
+import com.cloud.util.LogAnnotation;
+import com.cloud.util.ratelimiter.ExtRateLimiter;
+
 public class CountController {
 
 	//调用各个count
+	
+	@Autowired
+	private IBussinessSumService bussinessSumService;
+	
+	/**
+	  * 查询总数
+	  * @param BussinessSumBOQ
+	  * @return Long
+	  * @throws Exception
+	  */
+	@LogAnnotation(module = "查询总数")
+	@PreAuthorize("hasAuthority('back:total:queryCount')")
+	@ExtRateLimiter(permitsPerSecond=20,timeout=5)
+	@GetMapping("/api-business/total/queryCount")
+	public Long queryCount(BussinessSumBOQ bussinessSumBOQ)throws Exception{
+		return this.bussinessSumService.queryCount(bussinessSumBOQ);
+	}
+	
+	
 }
