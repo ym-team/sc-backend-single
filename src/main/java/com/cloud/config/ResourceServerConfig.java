546d71b9183fec7e27acc88e6c7d87ba59f4aeec
michael <mengdekaituozhe@163.com>
2019-09-21 12:05:59 +0800

    file upload

--- src/main/java/com/cloud/config/ResourceServerConfig.java
+++ src/main/java/com/cloud/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.cloud.config;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,13 @@ import com.cloud.util.PermitAllUrl;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	
+	/**
+	 * url前缀
+	 */
+	@Value("${file.local.prefix}")
+	public String localFilePrefix;
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().exceptionHandling()
@@ -31,7 +39,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 				.and().authorizeRequests()
 				.antMatchers(PermitAllUrl.permitAllUrl("/backend-anon/**", "/favicon.ico", "/css/**", "/js/**","/fonts/**", "/layui/**", "/img/**", "/pages/**", "/pages/**/*.html", "/*.html"
                         								,"/users-anon/**", "/wechat/**"
-                        								,"/notification-anon/**","/druid/**")).permitAll() // 放开权限的url
+                        								,"/notification-anon/**","/druid/**",
+                        								"/files-anon/**", localFilePrefix + "/**")).permitAll() // 放开权限的url
 				.anyRequest().authenticated().and().httpBasic();
 		
 		
